b0309b056d566a359166f12fd1422a66c7fa6643
Nick dos Remedios <nickdos@gmail.com>
2017-03-15 16:57:25 +1100

    Version 0.7.9-SNAPSHOT

--- ImagesClientPluginGrailsPlugin.groovy
+++ ImagesClientPluginGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ImagesClientPluginGrailsPlugin {
     // the plugin version
-    def version = "0.7.8"
+    def version = "0.7.9-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
