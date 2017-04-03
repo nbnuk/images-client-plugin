ef0fd6eb6de41872395e114338c755616afc08ee
Patricia Koh <patricia.koh@csiro.au>
2017-04-03 15:49:40 +1000

    Check for null in config

--- grails-app/taglib/images/client/plugin/ImageTagLib.groovy
+++ grails-app/taglib/images/client/plugin/ImageTagLib.groovy
@@ -6,7 +6,7 @@ class ImageTagLib {
     static namespace = 'imageTag'
 
     def checkAllowableEditRole = { attrs ->
-        List<String> allowedRoles = grailsApplication.config.allowedImageEditingRoleList.split(',')
+        List<String> allowedRoles = grailsApplication.config.get("allowedImageEditingRoles")?.split(",")?:[]
         def currentUserRoles = authService?.getUserId() ? authService.getUserForUserId(authService.getUserId())?.roles : []
         boolean match = currentUserRoles.any {allowedRoles.contains(it)}
         out << match;
