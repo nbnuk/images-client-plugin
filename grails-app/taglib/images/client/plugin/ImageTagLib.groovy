576c61e2c5d9c192afc51139fcc2d95b62841b33
Patricia Koh <patricia.koh@csiro.au>
2017-04-03 14:18:39 +1000

    Add configurable Allowed Roles for users who are allowed to nominate preferred species image

---
+++ grails-app/taglib/images/client/plugin/ImageTagLib.groovy
@@ -0,0 +1,15 @@
+package images.client.plugin
+
+class ImageTagLib {
+
+    def authService
+    static namespace = 'imageTag'
+
+    def checkAllowableEditRole = { attrs ->
+        List<String> allowedRoles = grailsApplication.config.allowedImageEditingRoleList.split(',')
+        def currentUserRoles = authService?.getUserId() ? authService.getUserForUserId(authService.getUserId())?.roles : []
+        boolean match = currentUserRoles.any {allowedRoles.contains(it)}
+        out << match;
+    }
+
+}
