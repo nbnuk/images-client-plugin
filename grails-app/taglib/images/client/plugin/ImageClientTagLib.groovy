a060a3e7aaa70629824e12a69bb7b16abc4b4592
Patricia Koh <patricia.koh@csiro.au>
2017-04-05 17:17:41 +1000

    Renamed namespace for taglib to imageClient for clarity and added javadoc remark

--- grails-app/taglib/images/client/plugin/ImageClientTagLib.groovy
+++ grails-app/taglib/images/client/plugin/ImageClientTagLib.groovy
@@ -1,10 +1,20 @@
 package images.client.plugin
 
-class ImageTagLib {
+/**
+ * Image Client Tag Lib to be used in the gsp related to image client plugin feature
+ *
+ */
+class ImageClientTagLib {
 
     def authService
-    static namespace = 'imageTag'
+    static namespace = 'imageClient'
 
+    /**
+     *
+     *  Outputs true if user is logged in and user role is in the allowable configured roles: allowedImageEditingRoles (Note that the IP must also match the authorised system IP for the user).
+     *  Otherwise, outputs false.
+     *
+     */
     def checkAllowableEditRole = { attrs ->
         List<String> allowedRoles = grailsApplication.config.get("allowedImageEditingRoles")?.split(",")?:[]
         def currentUserRoles = authService?.getUserId() ? authService.getUserForUserId(authService.getUserId())?.roles : []
