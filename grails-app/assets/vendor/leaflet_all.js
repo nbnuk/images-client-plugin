5e537fd02a15cb09b9ae1e6db0cd72d7e29823d1
Javier Molina <Javier.Molina@acsmail.net.au>
2017-09-15 09:37:23 +1000

    AtlasOfLivingAustralia/maintenance#63 - Fixing various asset loading issues

--- grails-app/assets/vendor/leaflet_all.js
+++ grails-app/assets/vendor/leaflet_all.js
@@ -1,6 +1,6 @@
 //= encoding UTF-8
 //= require leaflet/leaflet
-//= require leaflet/leaflet.muasure
+//= require leaflet/leaflet.measure
 //= require leaflet/Control.FullScreen
 //= require_tree leaflet.draw
 //= require_tree leaflet.loading
