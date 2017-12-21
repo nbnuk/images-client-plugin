e0c658f8b4184127f213acdcdaeccc08233c4a63
Nick dos Remedios <nick.dosremedios@csiro.au>
2017-12-21 15:39:39 +1100

    Fixed typo in JS include

--- grails-app/assets/vendor/leaflet_all.js
+++ grails-app/assets/vendor/leaflet_all.js
@@ -1,6 +1,6 @@
 //= encoding UTF-8
-//= require leaflet/leaflet
-//= require leaflet/leaflet.muasure
-//= require leaflet/Control.FullScreen
+//= require leaflet/leaflet.js
+//= require leaflet/leaflet.measure.js
+//= require leaflet/Control.FullScreen.js
 //= require_tree leaflet.draw
 //= require_tree leaflet.loading
